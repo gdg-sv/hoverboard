796c00c98a8952442fd70e70d855f21944dd0785
Suhas Deshpande <suhas2u@gmail.com>
2017-08-27 13:26:36 -0700

    cleanup

--- scripts/helper/schedule-webworker.js
+++ scripts/helper/schedule-webworker.js
@@ -1,27 +1,37 @@
 function isDefined(v) {
-  return v !== undefined;
+  return v !== undefined
 }
 
 function clone(obj) {
-  return JSON.parse(JSON.stringify(obj));
+  return JSON.parse(JSON.stringify(obj))
 }
 
 function findSpeakerById(id, speakerList) {
-  return speakerList.filter(function(s) {return s.id === +id;})[0];
+  return speakerList.filter(function(s) {
+    return s.id === +id
+  })[0]
 }
 
 function attachSessionAndSpeakersTogether(session, speakersRaw) {
+  console.log(session)
   if (isDefined(session.speakers)) {
-    for (var speakerIdx = 0; speakerIdx < session.speakers.length; speakerIdx++) {
+    for (
+      var speakerIdx = 0;
+      speakerIdx < session.speakers.length;
+      speakerIdx++
+    ) {
       if (!isDefined(session.speakers[speakerIdx].id)) {
-        session.speakers[speakerIdx] = findSpeakerById(session.speakers[speakerIdx], speakersRaw);
-        var tempSession = clone(session);
-        delete tempSession.speakers;
+        session.speakers[speakerIdx] = findSpeakerById(
+          session.speakers[speakerIdx],
+          speakersRaw
+        )
+        var tempSession = clone(session)
+        delete tempSession.speakers
         if (isDefined(session.speakers[speakerIdx])) {
           if (!isDefined(session.speakers[speakerIdx].sessions)) {
-            session.speakers[speakerIdx].sessions = [];
+            session.speakers[speakerIdx].sessions = []
           }
-          session.speakers[speakerIdx].sessions.push(tempSession);
+          session.speakers[speakerIdx].sessions.push(tempSession)
         }
       }
     }
@@ -33,68 +43,104 @@ function getEndTime(date, startTime, endTime, totalNumber, number) {
     timeStart = new Date(date + ' ' + startTime + ' ' + timezone).getTime(),
     timeEnd = new Date(date + ' ' + endTime + ' ' + timezone).getTime(),
     difference = Math.floor((timeEnd - timeStart) / totalNumber),
-    result = new Date(timeStart + difference * number);
-  return result.getHours() + ':' + result.getMinutes();
+    result = new Date(timeStart + difference * number)
+  return result.getHours() + ':' + result.getMinutes()
 }
 
 function hasSpeakersAndSessionsAndSchedule(speakers, sessions, schedule) {
-  return hasSpeakersAndSessions(speakers, sessions) && Object.keys(schedule).length ;
+  return (
+    hasSpeakersAndSessions(speakers, sessions) && Object.keys(schedule).length
+  )
 }
 
 function hasSpeakersAndSessions(speakers, sessions) {
-  return Object.keys(speakers).length && Object.keys(sessions).length;
+  return Object.keys(speakers).length && Object.keys(sessions).length
 }
 
 function addTagTo(tag, tags) {
   if (tags.indexOf(tag) < 0) {
-    tags.push(tag);
+    tags.push(tag)
   }
 }
 
-self.addEventListener('message', function (e) {
-  var speakers = e.data.speakers;
-  var sessions = e.data.sessions;
-  var schedule = e.data.schedule;
+self.addEventListener(
+  'message',
+  function(e) {
+    var speakers = e.data.speakers
+    var sessions = e.data.sessions
+    var schedule = e.data.schedule
 
-  if (hasSpeakersAndSessionsAndSchedule(speakers, sessions, schedule)) {
-    schedule.tags = [];
-    for (var dayIdx = 0, scheduleLen = schedule.length; dayIdx < scheduleLen; dayIdx++) {
-      var day = schedule[dayIdx];
-      day.tags = [];
-      for (var timeSlotIdx = 0, timeslotsLen = day.timeslots.length; timeSlotIdx < timeslotsLen; timeSlotIdx++) {
-        var timeslot = day.timeslots[timeSlotIdx];
-        for (var sessionIndex = 0, sessionsLen = timeslot.sessions.length; sessionIndex < sessionsLen; sessionIndex++) {
-          for (var subSessIdx = 0, subSessionsLen = timeslot.sessions[sessionIndex].length; subSessIdx < subSessionsLen; subSessIdx++) {
-            var session = sessions[timeslot.sessions[sessionIndex][subSessIdx]];
-            session.mainTag = session.tags ? session.tags[0] : 'General';
-            session.day = dayIdx + 1;
+    if (hasSpeakersAndSessionsAndSchedule(speakers, sessions, schedule)) {
+      schedule.tags = []
+      for (
+        var dayIdx = 0, scheduleLen = schedule.length;
+        dayIdx < scheduleLen;
+        dayIdx++
+      ) {
+        var day = schedule[dayIdx]
+        day.tags = []
+        for (
+          var timeSlotIdx = 0, timeslotsLen = day.timeslots.length;
+          timeSlotIdx < timeslotsLen;
+          timeSlotIdx++
+        ) {
+          var timeslot = day.timeslots[timeSlotIdx]
+          for (
+            var sessionIndex = 0, sessionsLen = timeslot.sessions.length;
+            sessionIndex < sessionsLen;
+            sessionIndex++
+          ) {
+            for (
+              var subSessIdx = 0,
+                subSessionsLen = timeslot.sessions[sessionIndex].length;
+              subSessIdx < subSessionsLen;
+              subSessIdx++
+            ) {
+              var session =
+                sessions[timeslot.sessions[sessionIndex][subSessIdx]]
+              session.mainTag = session.tags ? session.tags[0] : 'General'
+              session.day = dayIdx + 1
 
-            addTagTo(session.mainTag, day.tags);
-            addTagTo(session.mainTag, schedule.tags);
+              addTagTo(session.mainTag, day.tags)
+              addTagTo(session.mainTag, schedule.tags)
 
-            if (!isDefined(session.track)) {
-              session.track = day.tracks[sessionIndex];
+              if (!isDefined(session.track)) {
+                session.track = day.tracks[sessionIndex]
+              }
+              session.startTime = timeslot.startTime
+              session.endTime =
+                subSessionsLen > 1
+                  ? getEndTime(
+                      day.date,
+                      timeslot.startTime,
+                      timeslot.endTime,
+                      subSessionsLen,
+                      subSessIdx + 1
+                    )
+                  : timeslot.endTime
+              session.dateReadable = day.dateReadable
+
+              attachSessionAndSpeakersTogether(session, speakers)
+
+              schedule[dayIdx].timeslots[timeSlotIdx].sessions[sessionIndex][
+                subSessIdx
+              ] = session
             }
-            session.startTime = timeslot.startTime;
-            session.endTime = subSessionsLen > 1 ? getEndTime(day.date, timeslot.startTime, timeslot.endTime, subSessionsLen, subSessIdx + 1) : timeslot.endTime;
-            session.dateReadable = day.dateReadable;
-
-            attachSessionAndSpeakersTogether(session, speakers);
-
-            schedule[dayIdx].timeslots[timeSlotIdx].sessions[sessionIndex][subSessIdx] = session;
           }
         }
       }
+    } else if (hasSpeakersAndSessions(speakers, sessions)) {
+      Object.keys(sessions).forEach(function(sessionId) {
+        return attachSessionAndSpeakersTogether(sessions[sessionId], speakers)
+      })
     }
-  } else if (hasSpeakersAndSessions(speakers, sessions)) {
-    Object.keys(sessions).forEach(function(sessionId) {
-      return attachSessionAndSpeakersTogether(sessions[sessionId], speakers)
-    });
-  }
 
-  self.postMessage({
-    speakers: speakers,
-    sessions: sessions,
-    schedule: Array.isArray(schedule) && Object.keys(sessions).length ? schedule : []
-  });
-}, false);
+    self.postMessage({
+      speakers: speakers,
+      sessions: sessions,
+      schedule:
+        Array.isArray(schedule) && Object.keys(sessions).length ? schedule : []
+    })
+  },
+  false
+)
